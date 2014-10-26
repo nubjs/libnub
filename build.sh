8155a2158300682894550f09f6adba4883f39151
Trevor Norris <trev.norris@gmail.com>
2014-10-25 23:53:28 -0700

    Add submodule init/update
    So users don't forget they need to grab the submodules, add those lines
    to the build script.

--- build.sh
+++ build.sh
@@ -2,5 +2,8 @@
 
 SRC_DIR="$( cd "$( dirname "$0" )" && pwd )"
 
+git submodule init
+git submodule update
+
 ./tools/gyp/gyp --depth=$SRC_DIR -Duv_library=static_library -Icommon.gypi \
   --generator-output=$SRC_DIR/out/ -Goutput_dir=$SRC_DIR/out -f make nub.gyp
