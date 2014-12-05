2021e075f7b4acc64f3943ec218acca2f54e56ef
Thorsten Lorenz <thlorenz@gmx.de>
2014-12-05 12:02:41 -0800

    build: allow overriding generator passed to gyp

--- build.sh
+++ build.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 SRC_DIR="$( cd "$( dirname "$0" )" && pwd )"
+GENERATOR=${GENERATOR:-make}
 
 git submodule init
 git submodule update
 
 ./tools/gyp/gyp --depth=$SRC_DIR -Duv_library=static_library -Icommon.gypi \
-  --generator-output=$SRC_DIR/out/ -Goutput_dir=$SRC_DIR/out -f make nub.gyp
+  --generator-output=$SRC_DIR/out/ -Goutput_dir=$SRC_DIR/out -f $GENERATOR nub.gyp
